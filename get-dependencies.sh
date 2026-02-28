b4c0426d62d4237b7c6a912ae4004b39a6fc9454
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-28 01:50:11 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm ristretto
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package gdk-pixbuf2-noglycin
+make-aur-package libjxl-noglycin
+make-aur-package libheif-noglycin
+make-aur-package libavif-noglycin
+make-aur-package librsvg-noglycin
 
 # If the application needs to be manually built that has to be done down here
 
