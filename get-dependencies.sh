3ce88cb67ec3139a05a2dac3b5bd95ba3d37355a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-23 03:34:03 -0400

    `glycin-ng` is now provided by the debloated packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,10 +10,10 @@ pacman -Syu --noconfirm ristretto doxygen
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano x265-mini ! llvm ! gdk-pixbuf ! librsvg
+get-debloated-pkgs --add-common --prefer-nano glycin-mini x265-mini ! llvm ! gdk-pixbuf ! librsvg
 
 # Comment this out if you need an AUR package
-make-aur-package glycin-ng
+# make-aur-package 
 
 # If the application needs to be manually built that has to be done down here
 
