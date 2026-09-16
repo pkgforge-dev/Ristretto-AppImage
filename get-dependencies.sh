00fb255abb13307939b0195fbbc01508c3107c72
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-16 18:24:11 -0400

    test gnome glycin

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm ristretto doxygen
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ! glycin
 
 # Comment this out if you need an AUR package
 # make-aur-package 
