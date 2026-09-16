d7416baea2ba8e02112b343cb0adc18ceba1ab39
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-16 03:13:54 -0400

    `get-debloated-pkgs` has the same behaviour by default

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,7 @@ pacman -Syu --noconfirm ristretto doxygen
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano glycin-mini x265-mini ! llvm ! gdk-pixbuf ! librsvg
+get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
 # make-aur-package 
